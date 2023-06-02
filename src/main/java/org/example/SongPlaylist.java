50b0aa8a47ead2d3de48a12cc5522c7d0a0610df
Garima <garima.maharaj.singh@sap.com>
2023-06-02 23:22:24 +0530

    Menu option handle for invalid input

--- src/main/java/org/example/SongPlaylist.java
+++ src/main/java/org/example/SongPlaylist.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 class SongPlaylist {
     public static void main(String[] args) {
         LinkedList playlist = new LinkedList();
-        Scanner input=new Scanner(System.in);
-        Scanner inputS=new Scanner(System.in);// this will be used for String
+        Scanner input = new Scanner(System.in);
+        Scanner inputS = new Scanner(System.in);// this will be used for String
+        int choice;
 
-        while (true) {
+        do {
             System.out.println("\nPlaylist Operations:\n");
             System.out.println("1. Add a song to the playlist");
             System.out.println("2. Delete a song from the playlist");
@@ -18,61 +19,67 @@ class SongPlaylist {
             System.out.println("6. Display playlist");
             System.out.println("7. Exit\n");
             System.out.print("Enter Menu Option:");
-            int choice = input.nextInt();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter Song Title: ");
-                    String title = inputS.nextLine();
-                    while(title.isBlank() || title==(String)" ")
-                    {
-                        System.out.println("Invalid title. Enter Song Title again: ");
-                        title = inputS.nextLine();
-                    }
-                    System.out.print("Enter Song Duration (mm:ss): ");
-                    String duration = inputS.nextLine();
-                    playlist.addSong(title, duration);
-                    break;
-                case 2:
-                    System.out.print("Enter the position of the song to delete: ");
+            if (input.hasNextInt()) {
+                choice = input.nextInt();
+                input.nextLine(); // Consume the newline character
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter Song Title: ");
+                        String title = inputS.nextLine();
+                        while (title.isBlank() || title == (String) " ") {
+                            System.out.println("Invalid title. Enter Song Title again: ");
+                            title = inputS.nextLine();
+                        }
+                        System.out.print("Enter Song Duration (mm:ss): ");
+                        String duration = inputS.nextLine();
+                        playlist.addSong(title, duration);
+                        break;
+                    case 2:
+                        System.out.print("Enter the position of the song to delete: ");
 //                    int position = input.nextInt();
 //                    playlist.deleteAtPos(position);
-                    try {
-                        String x = inputS.nextLine();
-                        int position = Integer.parseInt(x);
-                        playlist.deleteAtPos(position);
-                    } catch (NumberFormatException e) {
-                        System.out.println("Invalid input.");
-                    }
-                    break;
-                case 3:
-                    System.out.print("Enter the Name of the Song: ");
-                    //scanner.nextLine();  // Consume the newline character
-                    String songName = inputS.nextLine();
-                    int position1 = playlist.findSong(songName);
-                    if (position1 != -1) {
-                        System.out.println("The Song is present at position " + position1);
-                    }
-                    break;
-                case 4:
-                    System.out.print("Enter a track number to start from: ");
-                    String start = inputS.nextLine();
-                    playlist.playTrack(start);
-                    break;
-                case 5:
-                    playlist.sortList();
-                    playlist.display();
-                    break;
-                case 6:
-                    playlist.display();
-                    break;
-                case 7:
-                    System.out.println("Exiting...");
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+                        try {
+                            String x = inputS.nextLine();
+                            int position = Integer.parseInt(x);
+                            playlist.deleteAtPos(position);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid input.");
+                        }
+                        break;
+                    case 3:
+                        System.out.print("Enter the Name of the Song: ");
+                        //scanner.nextLine();  // Consume the newline character
+                        String songName = inputS.nextLine();
+                        int position1 = playlist.findSong(songName);
+                        if (position1 != -1) {
+                            System.out.println("The Song is present at position " + position1);
+                        }
+                        break;
+                    case 4:
+                        System.out.print("Enter a track number to start from: ");
+                        String start = inputS.nextLine();
+                        playlist.playTrack(start);
+                        break;
+                    case 5:
+                        playlist.sortList();
+                        playlist.display();
+                        break;
+                    case 6:
+                        playlist.display();
+                        break;
+                    case 7:
+                        System.out.println("Exiting...");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a valid menu option number.");
+                input.nextLine(); // Consume the invalid input
+                choice = -1; // Set option to an invalid value to continue the loop
             }
-        }
+        } while (choice != 7);
     }
 }
